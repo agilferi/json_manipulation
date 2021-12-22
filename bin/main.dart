af9618d515275747e20b8c6825bb0ef051f60a10
agil feri <agilferi74@gmail.com>
2021-12-22 16:16:46 +0700

    Initial Commit

---
+++ bin/main.dart
@@ -0,0 +1,89 @@
+import 'dart:developer';
+
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+void main(List<String> arguments) async {
+  await initializeDateFormatting('id_ID');
+
+  final List<Map<String, dynamic>> _listOfJson = [
+    {
+      "inventory_id": 9382,
+      "name": "Brown Chair",
+      "type": "furniture",
+      "tags": ["chair", "furniture", "brown"],
+      "purchased_at": 1579190471,
+      "placement": {"room_id": 3, "name": "Meeting Room"}
+    },
+    {
+      "inventory_id": 9380,
+      "name": "Big Desk",
+      "type": "furniture",
+      "tags": ["desk", "furniture", "brown"],
+      "purchased_at": 1579190642,
+      "placement": {"room_id": 3, "name": "Meeting Room"}
+    },
+    {
+      "inventory_id": 2932,
+      "name": "LG Monitor 50 inch",
+      "type": "electronic",
+      "tags": ["monitor"],
+      "purchased_at": 1579017842,
+      "placement": {"room_id": 3, "name": "Lavender"}
+    },
+    {
+      "inventory_id": 232,
+      "name": "Sharp Pendingin Ruangan 2PK",
+      "type": "electronic",
+      "tags": ["ac"],
+      "purchased_at": 1578931442,
+      "placement": {"room_id": 5, "name": "Dhanapala"}
+    },
+    {
+      "inventory_id": 9382,
+      "name": "Alat Makan",
+      "type": "tableware",
+      "tags": ["spoon", "fork", "tableware"],
+      "purchased_at": 1578672242,
+      "placement": {"room_id": 10, "name": "Rajawali"}
+    }
+  ];
+
+  log('==== Find items in the Meeting Room ====');
+  for (final Map<String, dynamic> item in _listOfJson) {
+    if (item['placement']['name'] == 'Meeting Room') {
+      log(item['name']);
+    }
+  }
+
+  log('==== Find all electronic devices ====');
+  for (final Map<String, dynamic> item in _listOfJson) {
+    if (item['type'] == 'electronic') {
+      log(item['name']);
+    }
+  }
+
+  log('==== Find all the furniture =====');
+  for (final Map<String, dynamic> item in _listOfJson) {
+    if (item['type'] == 'furniture') {
+      log(item['name']);
+    }
+  }
+
+  log('==== Find all items were purchased on 16 Januari 2020 ====');
+  for (final Map<String, dynamic> item in _listOfJson) {
+    final _date =
+        DateTime.fromMillisecondsSinceEpoch(item['purchased_at'] * 1000);
+    if (DateFormat('dd LLLL yyyy', 'id_ID').format(_date) ==
+        '16 Januari 2020') {
+      log(item['name']);
+    }
+  }
+
+  log('==== Find all items with brown color ====');
+  for (final Map<String, dynamic> item in _listOfJson) {
+    if (item['tags'].contains('brown')) {
+      log(item['name']);
+    }
+  }
+}
